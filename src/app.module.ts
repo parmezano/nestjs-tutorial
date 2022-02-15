cab884f8b69e0fe71f8b5aca2c59bcf42e8fd5fc
parmezano <danilchick1612@gmail.com>
2022-02-15 16:54:18 +0200

    message

---
+++ src/app.module.ts
@@ -0,0 +1,31 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+
+const environment = process.env.NODE_ENV || 'development';
+
+@Module({
+  imports: [
+    UserModule,
+    AuthModule,
+
+    ConfigModule.forRoot({
+      envFilePath: `.env.${environment}`,
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(
+      process.env.MONGODB_WRITE_CONNECTION_STRING,
+      {
+        useNewUrlParser: true,
+        useUndefinedTopology: true,
+      }
+    )
+  ],
+})
+export class AppModule { }
